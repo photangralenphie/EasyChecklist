01a0b24dcda35e66cd947dcfbdd0d1bb779d4604
Jonas Helmer <64361476+photangralenphie@users.noreply.github.com>
2022-10-07 18:11:43 +0200

    Initial Commit

---
+++ EasyChecklist/EasyChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyChecklistApp.swift
+//  EasyChecklist
+//
+//  Created by Jonas Helmer on 07.10.22.
+//
+
+import SwiftUI
+
+@main
+struct EasyChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
